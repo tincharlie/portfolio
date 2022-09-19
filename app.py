f82d2dfc6eb5721c424a70e50e56d4371d75aaa0
tincharlie <vipulshukla999@gmail.com>
2022-09-19 23:12:58 +0530

    Commit 11:12PM

--- app.py
+++ app.py
@@ -6,11 +6,19 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
+    """
+    CreatedBy: Tincharlie
+    :return: Render the templates which we have created to show the home.
+    """
     return render_template('index.html', title='index')
 
 
 @app.route("/contact")
 def contact():
+    """
+    CreatedBy: Tincharlie
+    :return: Render templates which we have created to show the contact html.
+    """
     return render_template('contact.html', title='contact')
 
 
